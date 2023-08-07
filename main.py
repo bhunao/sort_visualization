b3d04654e509e4d9be7f61d2ee2a862fad7271fd
bhunao <bhunao@gmail.com>
2023-08-07 13:38:37 -0300

    working commit

--- main.py
+++ main.py
@@ -1,72 +1,79 @@
 import pyxel
-from random import choice
 
 
-pyxel.init(10, 18)
-
-table = [[0 for _ in range(18)] for _ in range(10)]
-piece = []
-pos = [2, 4]
-t = [
-    [0, 1, 0],
-    [1, 1, 1],
-]
-i = [[0, 0, 0, 0],]
-piece = t.copy()
+coisas = []
+for n in "01234":
+    for p1 in "CDEFGAB":
+        coisas.append(f"{p1}{n}")
 
 
-def get_blocks_pos(piece, pos):
-    x, y = pos
-    positions = []
-    for i, line in enumerate(piece):
-        for j, ll in enumerate(line):
-            if ll == 1:
-                positions.append((x+j, y+i))
-    return positions
+pyxel.init(200, 150)
+screen_range = range(10, pyxel.width-10, pyxel.width//len(coisas)-1)
+
+bars_size = [pyxel.rndi(0, 100) for size in screen_range][::-1]
+sorted_sizes = sorted(bars_size)
+xses = [x for x in screen_range]
+shiny_bar = 0
 
 
-def adjust_piece():
-    global piece, table, pos, t, i
-    FALL_TIME = 10
-    if pyxel.frame_count % FALL_TIME:
-        return
+def bar(pos: int, size: int, col=15):
+    pyxel.rect(
+        x=pos,
+        y=pyxel.height-20-size,
+        w=3,
+        h=size,
+        col=col,
+    )
 
-    if len(piece) + pos[1] - 1 > 16:
-        for x, y in get_blocks_pos(piece, pos):
-            table[x][y] = 9
-            pos = [5, 0]
-            piece = i.copy()
-        return
-    pos[1] += 1
+
+def play_bar():
+    global shiny_bar
+
+    sound_i = round(shiny_bar*.34)
+    pyxel.sound(0).set(
+        coisas[sound_i],
+        "p",
+        "6",
+        "v",
+        25,
+    )
+    pyxel.play(0, 0)
+
+
+def kinda_of_sort(lista: list):
+    global shiny_bar
+
+    lst_len = len(lista)
+    for i in range(lst_len):
+        for j in range(i, lst_len):
+            shiny_bar = j
+            if pyxel.sin(lista[i]) > pyxel.sin(lista[j]):
+                lista[i], lista[j] = lista[j], lista[i]
+                print(lista[i], lista[j])
+                play_bar()
+                return
 
 
 def draw():
+    global bars_size, shiny_bar
+
     pyxel.cls(1)
 
-    x, y = pos
-    for x, y in get_blocks_pos(piece, pos):
-        pyxel.pset(x, y, pyxel.frame_count % 15)
-
-    for x, d1 in enumerate(table):
-        for y, col in enumerate(d1):
-            if col:
-                pyxel.pset(x, y, col)
+    for i, (size, x) in enumerate(zip(bars_size, xses)):
+        if i == shiny_bar - 1:
+            col = pyxel.frame_count % 15
+        else:
+            col = 15
+        bar(x, size, col=col)
 
 
 def update():
-    global piece
-    if pyxel.btnp(pyxel.KEY_W):
-        pos[1] -= 1
-    if pyxel.btnp(pyxel.KEY_S):
-        pos[1] += 1
-    if pyxel.btnp(pyxel.KEY_A):
-        pos[0] -= 1
-    if pyxel.btnp(pyxel.KEY_D):
-        pos[0] += 1
-    if pyxel.btnp(pyxel.KEY_SPACE):
-        piece = list(zip(*piece[::-1]))
-    adjust_piece()
-    print(pos)
+    global shiny_bar, bars_size
+    if bars_size == sorted_sizes:
+        pass
+    elif pyxel.frame_count % 3 == 0:
+        # shiny_bar = (shiny_bar + 1) % len(bars_size)
+        kinda_of_sort(bars_size)
 
 
 pyxel.run(update, draw)
