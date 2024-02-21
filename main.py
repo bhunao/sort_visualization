68b9f40ddf50589e950f52b299720035a30a42af
bhunao <bh.felix@hotmail.com>
2024-02-21 11:14:49 -0300

    mais

--- main.py
+++ main.py
@@ -1,80 +1,42 @@
 import pyxel
 
 
-coisas = []
-for n in "01234":
-    for p1 in "CDEFGAB":
-        coisas.append(f"{p1}{n}")
+X, Y = 130, 110
+B_SIZE = 5
+pyxel.init(X, Y)
 
 
-pyxel.init(200, 150)
-screen_range = range(10, pyxel.width-10, pyxel.width//len(coisas)-1)
-
-bars_size = [pyxel.rndi(0, 100) for size in screen_range][::-1]
-sorted_sizes = sorted(bars_size)
-xses = [x for x in screen_range]
-shiny_bar = 0
+bars = [x for x in range(0, 100, 100//20)]
+bars = [pyxel.rndi(0, 100) for _ in range(20)]
+print(bars)
 
 
-def bar(pos: int, size: int, col=15):
-    pyxel.rect(
-        x=pos,
-        y=pyxel.height-20-size,
-        w=3,
-        h=size,
-        col=col,
-    )
-
-
-def play_bar():
-    global shiny_bar
-
-    sound_i = round(shiny_bar*.34)
-    pyxel.sound(0).set(
-        coisas[sound_i],
-        "p",
-        "6",
-        "v",
-        25,
-    )
-    pyxel.play(0, 0)
-
-
-def kinda_of_sort(lista: list):
-    global shiny_bar
-
-    lst_len = len(lista)
-    for i in range(lst_len):
-        for j in range(i, lst_len):
-            shiny_bar = j
-            play_bar()
-            if pyxel.sin(lista[i]) > pyxel.sin(lista[j]):
-                lista[i], lista[j] = lista[j], lista[i]
-                return
+def _sort():
+    global bars
+    size = len(bars)
+    last = bars[-1]
+    print("_sort")
+    for i in range(0, size-2):
+        if last < bars[i]:
+            yield
+            last, bars[i] = bars[i], last
 
 
 def draw():
-    global bars_size, shiny_bar
-
     pyxel.cls(1)
-
-    for i, (size, x) in enumerate(zip(bars_size, xses)):
-        if i == shiny_bar - 1:
-            col = pyxel.frame_count % 15
-        else:
-            col = 15
-        bar(x, size, col=col)
+    for i, b in enumerate(bars):
+        w, h = B_SIZE, b
+        x, y = (i+1) * (B_SIZE+1), 90-h
+        pyxel.rect(x, y, w, h, 10)
+        pyxel.text(x, y, str(b), 9)
 
 
 def update():
-    global shiny_bar, bars_size
-    if bars_size == sorted_sizes:
-        if pyxel.frame_count % 3 == 0:
-            shiny_bar = (shiny_bar + 1) % len(bars_size)
-            play_bar()
-    elif pyxel.frame_count % 3 == 0:
-        # shiny_bar = (shiny_bar + 1) % len(bars_size)
-        kinda_of_sort(bars_size)
+    global bars
+    if pyxel.frame_count % 10:
+        return
+    _sort()
+    print("update")
 
 
 pyxel.run(update, draw)
